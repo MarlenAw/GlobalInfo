c0b72abfeb6a741418a540a2f054c6b795f0cbe9
Marlen Awwad <marlenawsh3@gmail.com>
2018-04-01 21:30:26 +0300

    Find out what day is Easter Sunday/Palm Sunday on a specific year - NOT an interview question

---
+++ javaScript Interview Questions/easterDay.js
@@ -0,0 +1,19 @@
+function Easter(Y) {
+    var C = Math.floor(Y/100); // Math.floor converts floating point numbers to integers
+    var N = Y - 19*Math.floor(Y/19);
+    var K = Math.floor((C - 17)/25);
+    var I = C - Math.floor(C/4) - Math.floor((C - K)/3) + 19*N + 15;
+    I = I - 30*Math.floor((I/30));
+    I = I - Math.floor(I/28)*(1 - Math.floor(I/28)*Math.floor(29/(I + 1))*Math.floor((21 - N)/11));
+    var J = Y + Math.floor(Y/4) + I + 2 - C + Math.floor(C/4);
+    J = J - 7*Math.floor(J/7);
+    var L = I - J;
+    var M = 3 + Math.floor((L + 40)/44);
+    var D = L + 28 - 31*Math.floor(M/4);
+
+    return padout(D) + '.' + padout(M); //The padout() function adds a leading zero if the number passed is less than 10
+}
+
+function padout(number) { return (number < 10) ? '0' + number : number; }
+
+console.log(Easter(2018));
